909b792809e57835e9b0e781f78b72d64069fd03
lilacar <lilia.alejandracardenas@gmail.com>
2025-08-22 16:08:11 -0600

    se hicieron cambios para lograr su funcionamiento

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,3 +1,7 @@
+//juego piedra, papel o tijeras, se juega entre dos jugadores. cada uno debe ingresas su opcion, se comparan las dos opciones
+//empate si ambos eligen lo mismo
+//gana el jugador segun las reglas del juego normal piedra le gana a tijeras, tijeras a papel y papel a piedra.
+
 import java.util.Scanner; //se importo el java.util.Scanner
 
 
@@ -6,14 +10,14 @@ public class Ejercicio4 {
 
 
     Scanner s = new Scanner(System.in);//se agrego el System.in
-    String j1 = s.nextLine();
-    String j2 = s.nextLine();
+   
+   
     
     System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    
+     String j1 = s.nextLine();
     
     System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
-   
+    String j2 = s.nextLine();
    
     if (j1.equals(j2)) {
         System.out.println("Empate");
@@ -30,6 +34,7 @@ public class Ejercicio4 {
         case "papel":
           if (j2.equals("piedra")) {//se cambio el == por equals
             g = 1;}
+            break; //se agrego el break
           
         case "tijeras"://se agrego la s
           if (j2.equals("papel")) {//se cambio el == por equals
@@ -37,6 +42,10 @@ public class Ejercicio4 {
           }
           break;
         default:
+          //
+          System.out.println("Entrada no valída.");
+          s.close();
+          return;
       }
           System.out.println("Gana el jugador " + g);
     }
